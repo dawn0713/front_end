70e1fabeb1346ce3478aa5b0ef286642f2d6c6a3
huyuanai <18919027903@163.com>
2021-04-09 16:08:18 +0800

    update

--- src/api/application.js
+++ src/api/application.js
@@ -7,27 +7,6 @@ export function fetchList(params) {
         params:params
   })
 }
-export function createApplication(data) {
-    return request({
-        url: '/api_proxy/apps/apps/',
-        method: 'post',
-        data: data
-    })
-}
-
-export function getApplication(id) {
-    return request({
-        url: '/api_proxy/apps/apps/'+id,
-        method: 'get'
-    })
-}
-export function updateApplication(id,data) {
-    return request({
-        url: '/api_proxy/apps/apps/'+id,
-        method: 'post',
-        data:data
-    })
-}
 
 export function deleteApplication(params) {
     return request({
--- src/components/Upload/singleUpload.vue
+++ src/components/Upload/singleUpload.vue
@@ -1,8 +1,12 @@
 <template> 
   <div>
     <el-upload
+<<<<<<< HEAD
       action="http://127.0.0.1:7082/upload/jupiter/txt"
       headers="Access-Control-Allow-Origin"
+=======
+      action="http://dawn2-oss.oss-cn-beijing.aliyuncs.com"
+>>>>>>> parent of cb57953... update upload
       :data="dataObj"
       list-type="picture"
       :multiple="false" :show-file-list="showFileList"
